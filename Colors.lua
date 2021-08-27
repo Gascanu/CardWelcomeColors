1473e5b6ab22b69368f7633a3b407cd6450c8b21
MysteryDeath <angelofdeath2041@gmail.com>
2021-08-27 23:01:53 +0300

    Create Colors.lua

---
+++ Colors.lua
@@ -0,0 +1,28 @@
+"default": {
+  "default": "#FFFFFF",
+  "subtle": "#E6E8E8"
+},
+"dark": {
+  "default": "#FFFFFF",
+  "subtle": "#E6E8E8"
+},
+"light": {
+  "default": "#B4B6B8",
+  "subtle": "#929596"
+},
+"accent": {
+  "default": "#CE2D4F", // this affects the "positive" action for some reason
+  "subtle": "#00A0D1"
+},
+"good": {
+  "default": "#44CF50",
+  "subtle": "#7FEB86"
+},
+"warning": {
+  default": "#FF5C4A",
+  "subtle": "#FF5C4A"
+},
+"attention": {
+  "default": "#FF5C4A",
+  "subtle": "#FF5C4A"
+},
